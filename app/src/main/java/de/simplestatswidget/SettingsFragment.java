19897e60fc96c4943f99d2444f4642ccbf8520f1
Chessmasterrr <webmaster@psdatatech.de>
2018-06-06 22:29:32 +0200

    added possibility to specify a phonenumber in the preferences

--- app/src/main/java/de/simplestatswidget/SettingsFragment.java
+++ app/src/main/java/de/simplestatswidget/SettingsFragment.java
@@ -1,7 +1,10 @@
 package de.simplestatswidget;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceFragment;
+import android.telephony.TelephonyManager;
 
 public class SettingsFragment extends PreferenceFragment {
 
@@ -10,5 +13,21 @@ public class SettingsFragment extends PreferenceFragment {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.settings);
+
+        // get current phonenumber
+        String number = "";
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            TelephonyManager tMgr = (TelephonyManager) getContext().getSystemService(Context.TELEPHONY_SERVICE);
+            try {
+                number = tMgr.getLine1Number();
+            } catch (SecurityException e) {
+                // missing permission to get the number
+                number = "";
+            }
+        }
+
+        // set phonenumber in settings
+        EditTextPreference phonennumberPreference = (EditTextPreference) findPreference("phonenumber");
+        phonennumberPreference.setText(number);
     }
 }
